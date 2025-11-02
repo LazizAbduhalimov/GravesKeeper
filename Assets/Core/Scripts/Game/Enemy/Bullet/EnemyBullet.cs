e527655728a39b1d06096499d957458efd65e1ac
LazizAbduhalimov <78848741+LazizAbduhalimov@users.noreply.github.com>
2025-11-02 13:31:56 +0500

    fix physics issues

--- Assets/Core/Scripts/Game/Enemy/Bullet/EnemyBullet.cs
+++ Assets/Core/Scripts/Game/Enemy/Bullet/EnemyBullet.cs
@@ -30,27 +30,25 @@ public class EnemyBullet : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Wall")
-        {
-            _bounces++;
-            if (_bounces >= _bouncesBeforeDie) RefreshBullet();
-
-            Vector3 normal = collision.contacts[0].normal;
-            normal.y = 0;
-            normal.Normalize();
-
-            Vector3 reflectDir = Vector3.Reflect(transform.forward, normal);
-            reflectDir = Quaternion.Euler(0f, Random.Range(-_angleError, _angleError), 0f) * reflectDir;
-            transform.forward = reflectDir;
-
-            _rb.linearVelocity = new Vector3(reflectDir.x, 0f, reflectDir.z) * _speed;
-        }
+        Debug.Log($"{collision.gameObject.tag}, {collision.gameObject.name}");
         if (collision.gameObject.tag == "Player")
         {
             Debug.Log("Player");
             //ЛОГИКА СТОЛКНОВЕНИЕ С ИГРОКОМ
         }
+        
+        _bounces++;
+        if (_bounces >= _bouncesBeforeDie) RefreshBullet();
 
+        Vector3 normal = collision.contacts[0].normal;
+        normal.y = 0;
+        normal.Normalize();
+
+        Vector3 reflectDir = Vector3.Reflect(transform.forward, normal);
+        reflectDir = Quaternion.Euler(0f, Random.Range(-_angleError, _angleError), 0f) * reflectDir;
+        transform.forward = reflectDir;
+
+        _rb.linearVelocity = new Vector3(reflectDir.x, 0f, reflectDir.z) * _speed;
     }
     private void RefreshBullet()
     {
